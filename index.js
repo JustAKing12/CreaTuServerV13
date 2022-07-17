5e411d63eebfa8a8c00f154e9d9f968ddf9ecc54
JustAKing_ <32112097+JustAKing12@users.noreply.github.com>
2022-07-17 12:59:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const {Client, Intents, Collection} = require('discord.js')
+require('dotenv').config()
+const config = require('./config.json')
+const i18n = require('i18n')
+const fs = require('fs')
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]})
+
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.data.name, command)
+}
+
+client.once('ready', () => {
+    console.log("Megumin funcionando correctamente.")
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return
+
+    const command = client.commands.get(interaction.commandName)
+
+    if (!command) return
+    
+    try{
+        await command.run(interaction)
+    }catch(e){
+        console.log(e)
+        return interaction.reply({content: "Error inesperado, revisa tu código."})
+    }
+})
+
+client.login(config.token)
